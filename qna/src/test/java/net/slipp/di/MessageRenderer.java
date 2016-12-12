b6b43e34cc3c0f79f0e3e05c433845514d12e831
foreverskhd <foreverskhd@FOREVERSKHD34D2>
2016-12-12 15:11:19 +0900

    1. setter메소드를 통해서 java 소스안에서 di를 적용시킨것

--- qna/src/test/java/net/slipp/di/MessageRenderer.java
+++ qna/src/test/java/net/slipp/di/MessageRenderer.java
@@ -1,13 +1,23 @@
 package net.slipp.di;
 
 public class MessageRenderer {
+	
+	private MessageProvider messageProvider;
+	
+	public void setMessageProvider(MessageProvider messageProvider){
+		this.messageProvider = messageProvider;
+	}
+	
 	public void render(){
-		MessageProvider mp = new HelloWorldMessageProvider();
-		System.out.println(mp.getMessage());
+		System.out.println(messageProvider.getMessage());
 	}
 	
 	public static void main(String[] args) {
 		MessageRenderer renderer = new MessageRenderer();
+		renderer.setMessageProvider(new HelloWorldMessageProvider());
+		renderer.render();
+		
+		renderer.setMessageProvider(new HiWorldMessageProvider());
 		renderer.render();
 	}
 
